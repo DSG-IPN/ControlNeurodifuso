afd5651a2efc737aef03eed0fdc97c24f5fee39b
KattMoreno <90066250+KattMoreno@users.noreply.github.com>
2021-09-10 00:20:32 -0500

    Create problema2.m

---
+++ problema2.m
@@ -0,0 +1,18 @@
+function problema2(Dominio, rango)
+
+% Create figure
+figure1 = figure;
+colormap(cool);
+
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+
+% Create plot
+plot(Dominio,rango,'Marker','diamond','LineWidth',4,...
+    'Color',[0.717647058823529 0.274509803921569 1]);
+title('Función de membresia')
+xlabel('Dominio');
+ylabel('Rango');
+box(axes1,'on');
+hold(axes1,'off');
