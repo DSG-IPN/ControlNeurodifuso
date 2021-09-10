3c0af58cf1869f2901b6abee044bebdcb37a6b84
SAGO-DSG96 <daniel.salazar@comercializadora-sago.com>
2021-09-10 00:48:55 -0500

    feat: Add solution to problem3 graph discrete variables

--- problema3.m
+++ problema3.m
@@ -2,5 +2,6 @@ function [] = problema3(dominioX, rango)
 %Prueba esta función con los conjuntos difusos (A, B) de la imagen 5 (OperDif5). 
 %de las notas NO2. Operaciones difusas que se encuentran en la página del curso.
     figure;
-    stem(dominioX, rango); title('Conjunto difuso');
+    stem(dominioX, rango); 
+    title('Conjunto difuso');
 end
